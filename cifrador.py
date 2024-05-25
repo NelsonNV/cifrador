39995c3238f9b340e0952c1bd057e8b7af4f9a54
NelsonNV <nelsonluisnv2@gmail.com>
2024-05-25 13:51:38 -0400

    refactor feat: se mejoran los argumentos y ahora cifra los archivos de una carpeta

--- cifrador.py
+++ cifrador.py
@@ -1,6 +1,7 @@
 import argparse
 import base64
 import os
+from tqdm import tqdm
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -23,29 +24,44 @@ def cifrar_descifrar(accion, clave, ruta):
     fernet = obtener_fernet(clave)
 
     if os.path.isdir(ruta):
-        archivos = os.listdir(ruta)
+        for dirpath, dirnames, archivos in os.walk(ruta):
+            for nombre_archivo in tqdm(archivos, desc="Procesando archivos", unit="archivo"):
+                archivo = os.path.join(dirpath, nombre_archivo)
+                transformar_archivo(accion, fernet, archivo)
     else:
-        archivos = [ruta]
+        transformar_archivo(accion, fernet, ruta)
 
-    for nombre_archivo in archivos:
-        with open(nombre_archivo, 'rb') as f:
-            datos = f.read()
+def transformar_archivo(accion, fernet, archivo):
+    with open(archivo, 'rb') as f:
+        datos = f.read()
 
-        if accion == 'cifrar':
-            datos_transformados = fernet.encrypt(datos)
-        else:
-            datos_transformados = fernet.decrypt(datos)
+    if accion == 'cifrar':
+        datos_transformados = fernet.encrypt(datos)
+    else:
+        datos_transformados = fernet.decrypt(datos)
 
-        with open(nombre_archivo, 'wb') as f:
-            f.write(datos_transformados)
+    with open(archivo, 'wb') as f:
+        f.write(datos_transformados)
+
+def obtener_clave(ruta):
+    if os.path.isfile(ruta):
+        with open(ruta, 'r') as f:
+            return f.read().strip()
+    else:
+        return ruta
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Cifra o descifra archivos.')
-    parser.add_argument('accion', choices=['cifrar', 'descifrar'], help='La acción a realizar: cifrar o descifrar.')
-    parser.add_argument('clave', help='La clave utilizada para cifrar o descifrar.')
-    parser.add_argument('ruta', help='La ruta del archivo o carpeta a cifrar o descifrar.')
+    parser.add_argument('-c', '--cifrar', action='store_true', help='Cifrar el archivo o carpeta.')
+    parser.add_argument('-d', '--descifrar', action='store_true', help='Descifrar el archivo o carpeta.')
+    parser.add_argument('-f', '--file', type=str, help='El archivo a cifrar o descifrar.')
+    parser.add_argument('-e', '--directory', type=str, help='La carpeta a cifrar o descifrar.')
+    parser.add_argument('-k', '--key', type=str, required=True, help='La clave utilizada para cifrar o descifrar.')
 
     args = parser.parse_args()
 
-    cifrar_descifrar(args.accion, args.clave, args.ruta)
+    accion = 'cifrar' if args.cifrar else 'descifrar'
+    ruta = args.file if args.file else args.directory
+    clave = obtener_clave(args.key)
 
+    cifrar_descifrar(accion, clave, ruta)
